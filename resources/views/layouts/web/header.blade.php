cc6e0d43f6090ceff27179768fd3fe26180dd402
hieu.nguyen.van <nguyen.van.hieu@framgia.com>
2017-03-21 16:36:20 +0700

    Remove unnecessary code lines in resources/views/layouts/web/header.blade.php

--- resources/views/layouts/web/header.blade.php
+++ resources/views/layouts/web/header.blade.php
@@ -49,38 +49,20 @@
                         <link href="/myplay/css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
                         <script src="/myplay/js/jquery.magnific-popup.js" type="text/javascript"></script>
                         <!--//pop-up-box -->
-                        <!-- <div id="small-dialog2" class="mfp-hide">
-                          <h3>Create Account</h3>
-                          <div class="social-sits">
-                            <div class="facebook-button">
-                              <a href="#">Connect with Facebook</a>
-                            </div>
-                            <div class="chrome-button">
-                              <a href="#">Connect with Google</a>
-                            </div>
-                            <div class="button-bottom">
-                              <p>Already have an account? <a href="#small-dialog" class="play-icon popup-with-zoom-anim">Login</a></p>
-                            </div>
-                          </div>
-                          <div class="signup">
-                            <form>
-                              <input type="text" class="email" placeholder="Mobile Number" maxlength="10" pattern="[1-9]{1}\d{9}" title="Enter a valid mobile number" />
-                              <input type="text" class="email" name="name" placeholder="Name">
-                            </form>
-                            <div class="continue-button">
-                              <a href="#small-dialog3" class="hvr-shutter-out-horizontal play-icon popup-with-zoom-anim">CONTINUE</a>
-                            </div>
-                          </div>
-                          <div class="clearfix"> </div>
-                        </div> -->
                         <div id="small-dialog2" class="mfp-hide">
                             <h3>Create Account</h3>
                             <div class="social-sits">
                                 <div class="facebook-button">
-                                    <a href="#">Connect with Facebook</a>
+                                    <a href="{{ action('Auth\SocialAuthController@redirect', [
+                                        'provider' => config('custom.provider.facebook')
+                                        ]) }}">Connect with Facebook
+                                    </a>
                                 </div>
                                 <div class="chrome-button">
-                                    <a href="#">Connect with Google</a>
+                                    <a href="{{ action('Auth\SocialAuthController@redirect', [
+                                        'provider' => config('custom.provider.google')
+                                        ]) }}">Connect with Google
+                                    </a>
                                 </div>
                                 <div class="button-bottom">
                                     <p>Already have an account? <a href="#small-dialog" class="play-icon popup-with-zoom-anim">Login</a></p>
@@ -108,78 +90,6 @@
                             </div>
                             <div class="clearfix"> </div>
                         </div>
-                        <div id="small-dialog7" class="mfp-hide">
-                            <h3>Create Account</h3>
-                            <div class="social-sits">
-                                <div class="facebook-button">
-                                    <a href="#">Connect with Facebook</a>
-                                </div>
-                                <div class="chrome-button">
-                                    <a href="#">Connect with Google</a>
-                                </div>
-                                <div class="button-bottom">
-                                    <p>Already have an account? <a href="#small-dialog" class="play-icon popup-with-zoom-anim">Login</a></p>
-                                </div>
-                            </div>
-                            <div class="signup">
-                                <form action="upload.html">
-                                    <input type="text" class="email" placeholder="Email" required="required" pattern="([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?" title="Enter a valid email"/>
-                                    <input type="password" placeholder="Password" required="required" pattern=".{6,}" title="Minimum 6 characters required" autocomplete="off" />
-                                    <input type="submit"  value="Sign In"/>
-                                </form>
-                            </div>
-                            <div class="clearfix"> </div>
-                        </div>
-                        <div id="small-dialog4" class="mfp-hide">
-                            <h3>Feedback</h3>
-                            <div class="feedback-grids">
-                                <div class="feedback-grid">
-                                    <p>Suspendisse tristique magna ut urna pellentesque, ut egestas velit faucibus. Nullam mattis lectus ullamcorper dui dignissim, sit amet egestas orci ullamcorper.</p>
-                                </div>
-                                <div class="button-bottom">
-                                    <p><a href="#small-dialog" class="play-icon popup-with-zoom-anim">Sign in</a> to get started.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="small-dialog5" class="mfp-hide">
-                            <h3>Help</h3>
-                            <div class="help-grid">
-                                <p>Suspendisse tristique magna ut urna pellentesque, ut egestas velit faucibus. Nullam mattis lectus ullamcorper dui dignissim, sit amet egestas orci ullamcorper.</p>
-                            </div>
-                            <div class="help-grids">
-                                <div class="help-button-bottom">
-                                    <p><a href="#small-dialog4" class="play-icon popup-with-zoom-anim">Feedback</a></p>
-                                </div>
-                                <div class="help-button-bottom">
-                                    <p><a href="#small-dialog6" class="play-icon popup-with-zoom-anim">Lorem ipsum dolor sit amet</a></p>
-                                </div>
-                                <div class="help-button-bottom">
-                                    <p><a href="#small-dialog6" class="play-icon popup-with-zoom-anim">Nunc vitae rutrum enim</a></p>
-                                </div>
-                                <div class="help-button-bottom">
-                                    <p><a href="#small-dialog6" class="play-icon popup-with-zoom-anim">Mauris at volutpat leo</a></p>
-                                </div>
-                                <div class="help-button-bottom">
-                                    <p><a href="#small-dialog6" class="play-icon popup-with-zoom-anim">Mauris vehicula rutrum velit</a></p>
-                                </div>
-                                <div class="help-button-bottom">
-                                    <p><a href="#small-dialog6" class="play-icon popup-with-zoom-anim">Aliquam eget ante non orci fac</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="small-dialog6" class="mfp-hide">
-                            <div class="video-information-text">
-                                <h4>Video information & settings</h4>
-                                <p>Suspendisse tristique magna ut urna pellentesque, ut egestas velit faucibus. Nullam mattis lectus ullamcorper dui dignissim, sit amet egestas orci ullamcorper.</p>
-                                <ol>
-                                    <li>Nunc vitae rutrum enim. Mauris at volutpat leo. Vivamus dapibus mi ut elit fermentum tincidunt.</li>
-                                    <li>Nunc vitae rutrum enim. Mauris at volutpat leo. Vivamus dapibus mi ut elit fermentum tincidunt.</li>
-                                    <li>Nunc vitae rutrum enim. Mauris at volutpat leo. Vivamus dapibus mi ut elit fermentum tincidunt.</li>
-                                    <li>Nunc vitae rutrum enim. Mauris at volutpat leo. Vivamus dapibus mi ut elit fermentum tincidunt.</li>
-                                    <li>Nunc vitae rutrum enim. Mauris at volutpat leo. Vivamus dapibus mi ut elit fermentum tincidunt.</li>
-                                </ol>
-                            </div>
-                        </div>
                         <script>
                             $(document).ready(function() {
                                 $('.popup-with-zoom-anim').magnificPopup({
@@ -193,7 +103,6 @@
                                     removalDelay: 300,
                                     mainClass: 'my-mfp-zoom-in'
                                 });
-
                             });
                         </script>
                     </div>
